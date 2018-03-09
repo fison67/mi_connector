686aaa70b08486be5b3fd5ff043c9bc11c8f1e9a
chals <fison67@nate.com>
2018-03-09 22:39:31 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ cd /app/mi_connector
 
 wget https://raw.githubusercontent.com/fison67/mi_connector/master/mi-connector.js
 wget https://raw.githubusercontent.com/fison67/mi_connector/master/config.yaml
+wget https://raw.githubusercontent.com/fison67/mi_connector/master/log.sh
+
+chmod 755 /app/mi_connector/log.sh
 
 npm install pm2 -g
 npm install miio yaml request http log4js js-yaml node-schedule
+
