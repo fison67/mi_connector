4035416812b9864739a248a04661432677649b59
chals <fison67@nate.com>
2018-03-24 23:00:56 +0900

    Create xiaomi-wallswitch.groovy

---
+++ devicetypes/fison67/xiaomi-wallswitch/xiaomi-wallswitch.groovy
@@ -0,0 +1,144 @@
+/**
+ *  Xiaomi Wall Switch1 (v.0.0.1)
+ *
+ * MIT License
+ *
+ * Copyright (c) 2018 fison67@nate.com
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+ 
+import groovy.json.JsonSlurper
+
+metadata {
+	definition (name: "Xiaomi Wall Switch1", namespace: "fison67", author: "fison67") {
+        capability "Switch"						
+         
+        attribute "switch", "string"
+        
+        attribute "lastCheckin", "Date"
+        
+        command "on"
+        command "off"
+	}
+
+	simulator { }
+
+	tiles {
+		multiAttributeTile(name:"switch", type: "generic", width: 6, height: 4, canChangeIcon: true){
+			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
+                attributeState "on", label:'${name}', action:"off", icon:"st.switches.light.on", backgroundColor:"#00a0dc", nextState:"turningOff"
+                attributeState "off", label:'${name}', action:"on", icon:"st.switches.light.off", backgroundColor:"#ffffff", nextState:"turningOn"
+                
+                attributeState "turningOn", label:'${name}', action:"off", icon:"st.switches.light.on", backgroundColor:"#00a0dc", nextState:"turningOff"
+                attributeState "turningOff", label:'${name}', action:"on", icon:"st.switches.light.off", backgroundColor:"#ffffff", nextState:"turningOn"
+			}
+            
+            tileAttribute("device.lastCheckin", key: "SECONDARY_CONTROL") {
+    			attributeState("default", label:'Updated: ${currentValue}',icon: "st.Health & Wellness.health9")
+            }
+		}
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+}
+
+def setInfo(String app_url, String id, String index) {
+	log.debug "${app_url}, ${id}"
+	state.app_url = app_url
+    state.id = id
+    state.deviceIndex = index
+}
+
+def setStatus(params){
+    log.debug "${params.key} >> ${params.data}"
+ 
+ 	switch(params.key){
+    case "power":
+ 		def power = params.data
+    	sendEvent(name:"switch", value: power)
+    	break;
+    }
+    
+    def now = new Date().format("yyyy-MM-dd HH:mm:ss", location.timeZone)
+    sendEvent(name: "lastCheckin", value: now)
+}
+
+def on(){
+    log.debug "On >> ${state.id}"
+    def body = [
+        "id": state.id,
+        "cmd": "power",
+        "data": "on",
+        "index": state.deviceIndex
+    ]
+    def options = makeCommand(body)
+    sendCommand(options, null)
+}
+
+def off(){
+	log.debug "Off >> ${state.id}"
+	def body = [
+        "id": state.id,
+        "cmd": "power",
+        "data": "off",
+        "index": state.deviceIndex
+    ]
+    def options = makeCommand(body)
+    sendCommand(options, null)
+}
+
+def callback(physicalgraph.device.HubResponse hubResponse){
+	def msg
+    try {
+        msg = parseLanMessage(hubResponse.description)
+		def jsonObj = new JsonSlurper().parseText(msg.body)
+        setStatus(jsonObj.state)
+    } catch (e) {
+        log.error "Exception caught while parsing data: "+e;
+    }
+}
+
+def updated() {
+}
+
+def sendCommand(options, _callback){
+	def myhubAction = new physicalgraph.device.HubAction(options, null, [callback: _callback])
+    sendHubCommand(myhubAction)
+}
+
+def makeCommand(body){
+	def options = [
+     	"method": "POST",
+        "path": "/control",
+        "headers": [
+        	"HOST": state.app_url,
+            "Content-Type": "application/json"
+        ],
+        "body":body
+    ]
+    return options
+}
