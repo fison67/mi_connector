b35a6107db13d74c895730765c1cb9b3e1a5fa31
chals <fison67@nate.com>
2018-04-04 00:47:07 +0900

    Update xiaomi-door.groovy

--- devicetypes/fison67/xiaomi-door.src/xiaomi-door.groovy
+++ devicetypes/fison67/xiaomi-door.src/xiaomi-door.groovy
@@ -33,13 +33,13 @@ metadata {
 	definition (name: "Xiaomi Door", namespace: "fison67", author: "fison67") {
         capability "Sensor"
         capability "Contact Sensor"
-         
-        attribute "door", "string"
-        attribute "battery", "string"
-        
+        capability "Battery"
+	capability "Refresh"
+               
         attribute "lastCheckin", "Date"
+        attribute "lastOpen", "Date"
+        attribute "lastClosed", "Date"
         
-        command "refresh"
 	}
 
 
@@ -47,23 +47,35 @@ metadata {
 	}
 
 	tiles {
-		multiAttributeTile(name:"door", type: "generic", width: 6, height: 4){
-			tileAttribute ("device.door", key: "PRIMARY_CONTROL") {
-               	attributeState "open", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#e86d13"
-            	attributeState "closed", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#00a0dc"
+		multiAttributeTile(name:"contact", type: "generic", width: 6, height: 4){
+			tileAttribute ("device.contact", key: "PRIMARY_CONTROL") {
+               	attributeState "open", label:'${name}', icon:"http://postfiles12.naver.net/MjAxODA0MDNfNTMg/MDAxNTIyNzI0MjgzMjk1.H97Au-OWeJ5aUpwpYbhu3P_H_cA0tMHz-EWpgDPmmTcg.Jcbknv16shFQ86cBtY-Z1n4Jx9P1WGWGpj4voOLxzV8g.PNG.shin4299/door_on1.png?type=w3", backgroundColor:"#e86d13"
+            	attributeState "closed", label:'${name}', icon:"https://postfiles.pstatic.net/MjAxODA0MDJfMTI3/MDAxNTIyNjcwOTc2NDgy.WVcwn0G7-BnyFTkk4pUxZ44j-810YDbVb81-A-52D1gg.X_0ijEFzbyu8IeYXU_fr0mVtS4v_4JbZncfmoFCPH5cg.PNG.shin4299/door_off.png?type=w3", backgroundColor:"#00a0dc"
 			}
+            tileAttribute("device.battery", key: "SECONDARY_CONTROL") {
+    			attributeState("default", label:'Battery: ${currentValue}%\n')
+            }
             tileAttribute("device.lastCheckin", key: "SECONDARY_CONTROL") {
-    			attributeState("default", label:'Last Update: ${currentValue}',icon: "st.Health & Wellness.health9")
+    			attributeState("default", label:'\nLast Update: ${currentValue}')
             }
 		}
-        
-        valueTile("battery", "device.battery", width: 2, height: 2) {
-            state "val", label:'${currentValue}%', defaultState: true
-        }
         
         standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
             state "default", label:"", action:"refresh", icon:"st.secondary.refresh"
         }
+        valueTile("lastOpen_label", "", decoration: "flat") {
+            state "default", label:'Last\nOpen'
+        }
+        valueTile("lastOpen", "device.lastOpen", decoration: "flat", width: 3, height: 1) {
+            state "default", label:'${currentValue}'
+        }
+        valueTile("lastClosed_label", "", decoration: "flat") {
+            state "default", label:'Last\nClosed'
+        }
+        valueTile("lastClosed", "device.lastClosed", decoration: "flat", width: 3, height: 1) {
+            state "default", label:'${currentValue}'
+        }
+
 	}
 }
 
@@ -80,9 +92,16 @@ def setInfo(String app_url, String id) {
 
 def setStatus(params){
 	log.debug "${params.key} : ${params.data}"
+	def now = new Date().format("yyyy-MM-dd HH:mm:ss", location.timeZone)
  	switch(params.key){
     case "contact":
-    	sendEvent(name:"door", value: (params.data == "true" ? "closed" : "open") )
+	if(params.data == "true"){
+    		sendEvent(name:"contact", value: "closed" )
+    		sendEvent(name:"lastClosed", value: now )
+	} else {
+    		sendEvent(name:"contact", value: "open" )
+    		sendEvent(name:"lastOpen", value: now )
+	}		
     	break;
     case "batteryLevel":
     	sendEvent(name:"battery", value: params.data)
