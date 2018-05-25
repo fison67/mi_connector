ea80f07dd156d88786fa3833ad4df67d67f5b0d1
chals <fison67@nate.com>
2018-05-25 23:04:57 +0900

    Add a timer

--- devicetypes/fison67/xiaomi-light.src/xiaomi-light.groovy
+++ devicetypes/fison67/xiaomi-light.src/xiaomi-light.groovy
@@ -1,5 +1,5 @@
 /**
- *  Xiaomi Light (v.0.0.1)
+ *  Xiaomi Light (v.0.0.2)
  *
  * MIT License
  *
@@ -25,6 +25,9 @@
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
+ * Version. 0.0.2 >> Add Timer by fison67
 */
 
 import groovy.json.JsonSlurper
@@ -45,6 +48,8 @@ metadata {
         
         attribute "lastCheckin", "Date"
          
+        command "setTimeRemaining"
+        command "stop"
 	}
 
 
@@ -98,8 +103,20 @@ metadata {
             state "default", label:'${currentValue}'
         }
         
-   	main (["switch2"])
-	details(["switch", "refresh", "lastOn_label", "lastOn", "lastOff_label","lastOff" ])       
+        valueTile("timer_label", "device.leftTime", decoration: "flat", width: 2, height: 1) {
+            state "default", label:'Set Timer\n${currentValue}'
+        }
+        
+        controlTile("time", "device.timeRemaining", "slider", height: 1, width: 1, range:"(0..120)") {
+	    	state "time", action:"setTimeRemaining"
+		}
+        
+        standardTile("tiemr0", "device.timeRemaining") {
+			state "default", label: "OFF", action: "stop", icon:"st.Health & Wellness.health7", backgroundColor:"#c7bbc9"
+		}
+        
+        main (["switch2"])
+        details(["switch", "refresh", "lastOn_label", "lastOn", "lastOff_label","lastOff", "timer_label", "time", "tiemr0" ])       
 	}
 }
 
@@ -115,16 +132,16 @@ def setInfo(String app_url, String id) {
 }
 
 def setStatus(params){
+//	log.debug "Status >> ${params}"
     def now = new Date().format("yyyy-MM-dd HH:mm:ss", location.timeZone)
  	switch(params.key){
     case "power":
-    	log.debug "MI >> power " + (params.data == "true" ? "on" : "off")
         if(params.data == "true"){
-    	sendEvent(name:"switch", value: "on")
-	    sendEvent(name: "lastOn", value: now)
+            sendEvent(name:"switch", value: "on")
+            sendEvent(name: "lastOn", value: now)
         } else {
-        sendEvent(name:"switch", value: "off")
-	    sendEvent(name: "lastOff", value: now)
+            sendEvent(name:"switch", value: "off")
+            sendEvent(name: "lastOff", value: now)
         }
     	break;
     case "color":
@@ -153,19 +170,26 @@ def refresh(){
 }
 
 def setLevel(brightness){
-	log.debug "setBrightness >> ${state.id}, val=${brightness}"
-    def body = [
-        "id": state.id,
-        "cmd": "brightness",
-        "data": brightness
-    ]
-    def options = makeCommand(body)
-    sendCommand(options, null)
+	log.debug "setBrightness >> ID(${state.id}), val=${brightness}"
+    
+    if(brightness == 0){
+    	off()
+    }else{
+        def body = [
+            "id": state.id,
+            "cmd": "brightness",
+            "data": brightness
+        ]
+        def options = makeCommand(body)
+        sendCommand(options, null)
+
+    	setPowerByStatus(true)
+    }
+    
 }
 
 def setColor(color){
-	log.debug "setColor >> ${state.id}"
-    log.debug "${color.hex}"
+	log.debug "setColor >> ${state.id} >> ${color.hex}"
     def body = [
         "id": state.id,
         "cmd": "color",
@@ -173,6 +197,8 @@ def setColor(color){
     ]
     def options = makeCommand(body)
     sendCommand(options, null)
+    
+    setPowerByStatus(true)
 }
 
 def on(){
@@ -206,9 +232,9 @@ def callback(physicalgraph.device.HubResponse hubResponse){
         msg = parseLanMessage(hubResponse.description)
 		def jsonObj = new JsonSlurper().parseText(msg.body)
         log.debug jsonObj
-        def colors = jsonObj.properties.color.values
-        String hex = String.format("#%02x%02x%02x", colors[0].toInteger(), colors[1].toInteger(), colors[2].toInteger());  
-    	sendEvent(name:"color", value: hex )
+        
+     	String hex = String.format("#%02x%02x%02x", jsonObj.state.colorRGB.red, jsonObj.state.colorRGB.blue, jsonObj.state.colorRGB.green);
+        sendEvent(name:"color", value: hex )
         sendEvent(name:"level", value: jsonObj.properties.brightness)
         sendEvent(name:"switch", value: jsonObj.properties.power == true ? "on" : "off")
 	    
@@ -236,4 +262,75 @@ def makeCommand(body){
         "body":body
     ]
     return options
+}
+
+def msToTime(duration) {
+    def seconds = (duration%60).intValue()
+    def minutes = ((duration/60).intValue() % 60).intValue()
+    def hours = ( (duration/(60*60)).intValue() %24).intValue()
+
+    hours = (hours < 10) ? "0" + hours : hours
+    minutes = (minutes < 10) ? "0" + minutes : minutes
+    seconds = (seconds < 10) ? "0" + seconds : seconds
+
+    return hours + ":" + minutes + ":" + seconds
+}
+
+def stop() { 
+	unschedule()
+	state.timerCount = 0
+	updateTimer()
+}
+
+def timer(){
+	if(state.timerCount > 0){
+    	state.timerCount = state.timerCount - 30;
+        if(state.timerCount <= 0){
+        	if(device.currentValue("switch") == "on"){
+        		off()
+            }
+        }else{
+        	runIn(30, timer)
+        }
+        updateTimer()
+    }
+//	log.debug "Left Time >> ${state.timerCount}"
+}
+
+def updateTimer(){
+    def timeStr = msToTime(state.timerCount)
+    sendEvent(name:"leftTime", value: "${timeStr}")
+    sendEvent(name:"timeRemaining", value: Math.round(state.timerCount/60))
+}
+
+def processTimer(second){
+	if(state.timerCount == null){
+    	state.timerCount = second;
+    	runIn(30, timer)
+    }else if(state.timerCount == 0){
+		state.timerCount = second;
+    	runIn(30, timer)
+    }else{
+    	state.timerCount = second
+    }
+//    log.debug "Left Time >> ${state.timerCount} seconds"
+    updateTimer()
+}
+
+def setTimeRemaining(time) { 
+	if(time > 0){
+        log.debug "Set a Timer ${time}Mins"
+        processTimer(time * 60)
+        setPowerByStatus(true)
+    }
+}
+
+def setPowerByStatus(turnOn){
+	if(device.currentValue("switch") == (turnOn ? "off" : "on")){
+        if(turnOn){
+        	on()
+        }else{
+        	off()
+        }
+    }
 }
