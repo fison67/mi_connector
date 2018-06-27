0d225a1aa90cf0ab5c4d1873536141ef0c3e27d6
chals <fison67@nate.com>
2018-06-27 16:58:32 +0900

    ADD a graph

--- devicetypes/fison67/xiaomi-power-plug.src/xiaomi-power-plug.groovy
+++ devicetypes/fison67/xiaomi-power-plug.src/xiaomi-power-plug.groovy
@@ -1,5 +1,5 @@
 /**
- *  Xiaomi Power Plug (v.0.0.1)
+ *  Xiaomi Power Plug (v.0.0.2)
  *
  * MIT License
  *
@@ -45,9 +45,19 @@ metadata {
         attribute "temp", "string"
         attribute "lastCheckin", "Date"
         
+        command "chartPower"
+        command "chartPowerMeter"
+        command "chartEnergyMeter"
 	}
 
 	simulator { }
+    
+    preferences {
+		input "historyDayCount", "number", title: "Day for History Graph", description: "", defaultValue:1, displayDuringSetup: true
+		input "historyPowerDataMaxCount", "number", title: "Power Graph Data Max Count", description: "0 is max", defaultValue:100, displayDuringSetup: true
+		input "historyPowerLoadDataMaxCount", "number", title: "PowerLoad Graph Data Max Count", description: "0 is max", defaultValue:0, displayDuringSetup: true
+		input "historyPowerConsumedDataMaxCount", "number", title: "Power Graph Data Max Count", description: "0 is max", defaultValue:0, displayDuringSetup: true   
+	}
 
 	tiles(scale: 2) {
 		multiAttributeTile(name:"switch", type: "generic", width: 6, height: 4){
@@ -79,11 +89,20 @@ metadata {
         	state "energyMeter", label: 'Energy\n${currentValue}KWh', action: "energy", defaultState: true
         }
         
-        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 1) {
             state "default", label:"", action:"refresh", icon:"st.secondary.refresh"
         }
+        
+        standardTile("chartMode", "device.chartMode", width: 2, height: 1, decoration: "flat") {
+			state "chartPower", label:'Power', nextState: "chartPowerMeter", action: 'chartPower'
+			state "chartPowerMeter", label:'Power Meter', nextState: "chartEnergyMeter", action: 'chartPowerMeter'
+			state "chartEnergyMeter", label:'Energy Meter', nextState: "chartPower", action: 'chartEnergyMeter'
+		}
+        
+        carouselTile("history", "device.image", width: 6, height: 4) { }
+        
         main (["switch"])
-        details(["switch", "powerMeter", "energyMeter", "refresh"])
+        details(["switch", "powerMeter", "energyMeter", "refresh", "chartMode", "history"])
         
 	}
 }
@@ -91,6 +110,11 @@ metadata {
 // parse events into attributes
 def parse(String description) {
 	log.debug "Parsing '${description}'"
+}
+
+def setExternalAddress(address){
+	log.debug "External Address >> ${address}"
+	state.externalAddress = address
 }
 
 def setInfo(String app_url, String id) {
@@ -170,11 +194,11 @@ def callback(physicalgraph.device.HubResponse hubResponse){
     try {
         msg = parseLanMessage(hubResponse.description)
 		def jsonObj = new JsonSlurper().parseText(msg.body)
-
-        sendEvent(name:"powerMeter", value: jsonObj.properties.powerLoad.value)
-        sendEvent(name:"battery", value: jsonObj.properties.batteryLevel)
-        sendEvent(name:"energyMeter", value: jsonObj.properties.powerConsumed.value/1000)
+		log.debug jsonObj
         
+		try{ sendEvent(name:"powerMeter", value: jsonObj.properties.powerLoad.value) }catch(err){}
+    //    try{ sendEvent(name:"battery", value: jsonObj.properties.batteryLevel) }catch(err){}
+        try{ sendEvent(name:"energyMeter", value: jsonObj.properties.powerConsumed.value/1000)  }catch(err){}
         
         updateLastTime()
     } catch (e) {
@@ -201,4 +225,73 @@ def makeCommand(body){
         "body":body
     ]
     return options
+}
+
+
+def makeURL(type, name){
+	def sDate
+    def eDate
+	use (groovy.time.TimeCategory) {
+      def now = new Date()
+      def day = settings.historyDayCount == null ? 1 : settings.historyDayCount
+      sDate = (now - day.days).format( 'yyyy-MM-dd HH:mm:ss', location.timeZone )
+      eDate = now.format( 'yyyy-MM-dd HH:mm:ss', location.timeZone )
+    }
+	return [
+        uri: "http://${state.externalAddress}",
+        path: "/devices/history/${state.id}/${type}/${sDate}/${eDate}/image",
+        query: [
+        	"name": name
+        ]
+    ]
+}
+
+def chartPower() {
+	def url = makeURL("power", "Power")
+    if(settings.historyPowerDataMaxCount > 0){
+    	url.query.limit = settings.historyPowerDataMaxCount
+    }
+    httpGet(url) { response ->
+    	processImage(response, "power")
+    }
+}
+
+def chartPowerMeter(){
+	def url = makeURL("powerLoad", "PowerLoad")
+    if(settings.historyPowerLoadDataMaxCount > 0){
+    	url.query.limit = settings.historyPowerLoadDataMaxCount
+    }
+    httpGet(url) { response ->
+    	processImage(response, "powerLoad")
+    }
+}
+
+def chartEnergyMeter(){
+	def url = makeURL("powerConsumed", "PowerConsumed")
+    if(settings.historyPowerConsumedDataMaxCount > 0){
+    	url.query.limit = settings.historyPowerConsumedDataMaxCount
+    }
+    httpGet(url) { response ->
+    	processImage(response, "powerConsumed")
+    }
+}
+
+def processImage(response, type){
+	if (response.status == 200 && response.headers.'Content-Type'.contains("image/png")) {
+        def imageBytes = response.data
+        if (imageBytes) {
+            try {
+                storeImage(getPictureName(type), imageBytes)
+            } catch (e) {
+                log.error "Error storing image ${name}: ${e}"
+            }
+        }
+    } else {
+        log.error "Image response not successful or not a jpeg response"
+    }
+}
+
+private getPictureName(type) {
+  def pictureUuid = java.util.UUID.randomUUID().toString().replaceAll('-', '')
+  return "image" + "_$pictureUuid" + "_" + type + ".png"
 }
