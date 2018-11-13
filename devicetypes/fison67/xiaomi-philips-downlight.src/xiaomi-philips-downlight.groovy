6fa8d72ea9fcb6a490f75557ae22f027e54220aa
chals <fison67@nate.com>
2018-11-13 22:13:45 +0900

    Update xiaomi-philips-downlight.groovy

--- devicetypes/fison67/xiaomi-philips-downlight.src/xiaomi-philips-downlight.groovy
+++ devicetypes/fison67/xiaomi-philips-downlight.src/xiaomi-philips-downlight.groovy
@@ -43,6 +43,10 @@ metadata {
         attribute "lastOff", "string"
         attribute "lastCheckin", "Date"
          
+        command "setScene1"
+        command "setScene2"
+        command "setScene3"
+        command "setScene4"
 	}
 
 	preferences {
@@ -100,9 +104,24 @@ metadata {
         valueTile("lastOff", "device.lastOff", decoration: "flat", width: 3, height: 1) {
             state "default", label:'${currentValue}'
         }
+        standardTile("scene1", "device.scene") {
+			state "default", label: "Bright", action: "setScene1", backgroundColor:"#f2aebc"
+		}
+        standardTile("scene2", "device.scene") {
+			state "default", label: "TV", action: "setScene2", backgroundColor:"#f77993"
+		}
+        standardTile("scene3", "device.scene") {
+			state "default", label: "Warm", action: "setScene3", backgroundColor:"#f75475"
+		}
+        standardTile("scene4", "device.scene") {
+			state "default", label: "Midnight", action: "setScene4", backgroundColor:"#f72751"
+		}
+        valueTile("modeName", "device.modeName", decoration: "flat", width: 2, height: 1) {
+            state "default", label:'${currentValue}'
+        }
         
    	main (["switch2"])
-	details(["switch", "refresh", "lastOn_label", "lastOn", "lastOff_label","lastOff" ])       
+	details(["switch", "refresh", "lastOn_label", "lastOn", "lastOff_label","lastOff", "scene1", "scene2", "scene3", "scene4", "modeName" ])       
 	}
 }
 
@@ -138,8 +157,33 @@ def setStatus(params){
     case "brightness":
     	sendEvent(name:"level", value: params.data )
     	break;
+    case "scene":
+        sendEvent(name:"modeName", value: getModeName(params.data as int) )
+    	break;
     }
     sendEvent(name: "lastCheckin", value: now, displayed: false)
+}
+
+def getModeName(val){
+	def name
+	switch(val){
+    case 0:
+    	name = "Custom"
+        break;
+    case 1:
+		name = "Bright"
+    	break;
+	case 2:
+		name = "TV"
+		break;
+	case 3:
+		name = "Warm"
+        break;
+    case 4:
+		name = "Midnight"
+        break;
+    }
+	return name
 }
 
 def refresh(){
@@ -201,18 +245,67 @@ def off(){
     sendCommand(options, null)
 }
 
+def setScene1(){
+	log.debug "setScene1 >> ${state.id}"
+    
+    def body = [
+        "id": state.id,
+        "cmd": "scene",
+        "data": 1
+    ]
+    def options = makeCommand(body)
+    sendCommand(options, null)
+}
+
+def setScene2(){
+	log.debug "setScene2 >> ${state.id}"
+    
+    def body = [
+        "id": state.id,
+        "cmd": "scene",
+        "data": 2
+    ]
+    def options = makeCommand(body)
+    sendCommand(options, null)
+}
+
+def setScene3(){
+	log.debug "setScene3 >> ${state.id}"
+    
+    def body = [
+        "id": state.id,
+        "cmd": "scene",
+        "data": 3
+    ]
+    def options = makeCommand(body)
+    sendCommand(options, null)
+}
+
+def setScene4(){
+	log.debug "setScene4 >> ${state.id}"
+    
+    def body = [
+        "id": state.id,
+        "cmd": "scene",
+        "data": 4
+    ]
+    def options = makeCommand(body)
+    sendCommand(options, null)
+}
+
 
 def callback(physicalgraph.device.HubResponse hubResponse){
 	def msg
     try {
         msg = parseLanMessage(hubResponse.description)
 		def jsonObj = new JsonSlurper().parseText(msg.body)
-        
-        def colorRGB = colorTemperatureToRGB(jsonObj.state.colorTemperature)
+        log.debug jsonObj
+        def colorRGB = colorTemperatureToRGB(jsonObj.state.color.values[0])
         String hex = String.format("#%02x%02x%02x", (int)colorRGB[0], (int)colorRGB[1], (int)colorRGB[2]);  
     	sendEvent(name:"color", value: hex )
-        sendEvent(name:"level", value: jsonObj.properties.brightness)
-        sendEvent(name:"switch", value: jsonObj.properties.power == true ? "on" : "off")
+        sendEvent(name:"level", value: jsonObj.state.brightness)
+        sendEvent(name:"switch", value: jsonObj.state.power == true ? "on" : "off")
+        sendEvent(name:"modeName", value: getModeName(jsonObj.state.scene) )
 	    
         def now = new Date().format("yyyy-MM-dd HH:mm:ss", location.timeZone)
         sendEvent(name: "lastCheckin", value: now, displayed: false)
