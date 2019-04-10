ea157a2ab279df966c40c9675b7a186d88b0291f
chals <fison67@nate.com>
2019-04-11 00:03:59 +0900

    Update xiaomi-power-plug.groovy

--- devicetypes/fison67/xiaomi-power-plug.src/xiaomi-power-plug.groovy
+++ devicetypes/fison67/xiaomi-power-plug.src/xiaomi-power-plug.groovy
@@ -31,7 +31,7 @@
 import groovy.json.JsonSlurper
 
 metadata {
-	definition (name: "Xiaomi Power Plug", namespace: "fison67", author: "fison67") {
+	definition (name: "Xiaomi Power Plug", namespace: "fison67", author: "fison67", vid: "generic-switch-power-energy", ocfDeviceType: "oic.d.smartplug") {
         capability "Actuator"
         capability "Switch"
         capability "Power Meter"
