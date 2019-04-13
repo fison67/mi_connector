d6d4fa4c54aadc5b254555b5b0c03429ea9b025a
chals <fison67@nate.com>
2019-04-13 18:43:10 +0900

    Update xiaomi-philips-bedside-lamp.groovy

--- devicetypes/fison67/xiaomi-philips-bedside-lamp.src/xiaomi-philips-bedside-lamp.groovy
+++ devicetypes/fison67/xiaomi-philips-bedside-lamp.src/xiaomi-philips-bedside-lamp.groovy
@@ -29,7 +29,6 @@
 
 import groovy.json.JsonSlurper
 
-import java.awt.Color
 import java.util.ArrayList
 
 metadata {
@@ -38,7 +37,9 @@ metadata {
         capability "Actuator"
         capability "Refresh"
         capability "Light"
+		capability "ColorTemperature"
 		capability "Color Control"
+		capability "ColorMode"
         capability "Switch Level"
 
         attribute "lastOn", "string"
@@ -58,82 +59,13 @@ metadata {
 	}
     
 	preferences {
+		input name:	"smooth", type:"enum", title:"Select", options:["On", "Off"], description:"", defaultValue: "On"
+        input name: "duration", title:"Duration" , type: "number", required: false, defaultValue: 500, description:""
 	}
-
+	
 	simulator {
 	}
 
-	tiles(scale: 2) {
-		multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4){
-			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
-                attributeState "on", label:'${name}', action:"switch.off", icon:"https://github.com/fison67/mi_connector/blob/master/icons/xiaomi-philips-bedside-lamp-on-50.png?raw=true", backgroundColor:"#00a0dc", nextState:"turningOff"
-                attributeState "off", label:'${name}', action:"switch.on", icon:"https://github.com/fison67/mi_connector/blob/master/icons/xiaomi-philips-bedside-lamp-off-50.png?raw=true", backgroundColor:"#ffffff", nextState:"turningOn"
-                
-                attributeState "turningOn", label:'${name}', action:"switch.off", icon:"https://github.com/fison67/mi_connector/blob/master/icons/xiaomi-philips-bedside-lamp-on-50.png?raw=true", backgroundColor:"#00a0dc", nextState:"turningOff"
-                attributeState "turningOff", label:'${name}', action:"switch.ofn", icon:"https://github.com/fison67/mi_connector/blob/master/icons/xiaomi-philips-bedside-lamp-off-50.png?raw=true", backgroundColor:"#ffffff", nextState:"turningOn"
-			}
-            
-            tileAttribute("device.lastCheckin", key: "SECONDARY_CONTROL") {
-    			attributeState("default", label:'Updated: ${currentValue}')
-            }
-            
-            tileAttribute ("device.level", key: "SLIDER_CONTROL") {
-                attributeState "level", action:"switch level.setLevel"
-            }
-            tileAttribute ("device.color", key: "COLOR_CONTROL") {
-                attributeState "color", action:"setColor"
-            }
-		}
-        valueTile("refresh", "device.refresh", width: 2, height: 2, decoration: "flat") {
-            state "default", label:'', action:"refresh", icon:"st.secondary.refresh"
-        }        
-        valueTile("lastOn_label", "", decoration: "flat") {
-            state "default", label:'Last\nON'
-        }
-        valueTile("lastOn", "device.lastOn", decoration: "flat", width: 3, height: 1) {
-            state "default", label:'${currentValue}'
-        }
-        valueTile("lastOff_label", "", decoration: "flat") {
-            state "default", label:'Last\nOFF'
-        }
-        valueTile("lastOff", "device.lastOff", decoration: "flat", width: 3, height: 1) {
-            state "default", label:'${currentValue}'
-        }
-        
-        standardTile("scene1", "device.scene") {
-			state "default", label: "Scene1", action: "setScene1", backgroundColor:"#2FFF13"
-		}
-        standardTile("scene2", "device.scene") {
-			state "default", label: "Scene2", action: "setScene2", backgroundColor:"#CF10FF"
-		}
-        standardTile("scene3", "device.scene") {
-			state "default", label: "Scene3", action: "setScene3", backgroundColor:"#FF2619"
-		}
-        standardTile("scene4", "device.scene") {
-			state "default", label: "Scene4", action: "setScene4", backgroundColor:"#32F2FF"
-		}
-        standardTile("scene5", "device.scene") {
-			state "default", label: "Scene5", action: "setScene5", backgroundColor:"#1B86FF"
-		}
-        standardTile("scene6", "device.scene") {
-			state "default", label: "Scene6", action: "setScene6", backgroundColor:"#FFCF97"
-		}
-        
-        valueTile("timer_label", "device.leftTime", decoration: "flat", width: 2, height: 1) {
-            state "default", label:'Set Timer\n${currentValue}'
-        }
-        
-        controlTile("time", "device.timeRemaining", "slider", height: 1, width: 1, range:"(0..120)") {
-	    	state "time", action:"setTimeRemaining"
-		}
-        
-        standardTile("tiemr0", "device.timeRemaining") {
-			state "default", label: "OFF", action: "stop", icon:"st.Health & Wellness.health7", backgroundColor:"#c7bbc9"
-		}
-        
-        main (["switch"])
-        details(["switch", "refresh", "lastOn_label", "lastOn", "lastOff_label","lastOff", "scene1", "scene2", "scene3", "scene4", "scene5", "scene6", "timer_label", "time", "tiemr0" ])       
-	}
 }
 
 // parse events into attributes
@@ -165,9 +97,7 @@ def setStatus(params){
         }
     	break;
     case "color":
-    	def colors = params.data.split(",")
-        String hex = String.format("#%02x%02x%02x", colors[0].toInteger(), colors[1].toInteger(), colors[2].toInteger());  
-    	sendEvent(name:"color", value: hex )
+    	sendEvent(name:"color", value: params.data )
     	break;
     case "brightness":
     	sendEvent(name:"level", value: params.data )
@@ -213,19 +143,30 @@ def setLevel(brightness){
     
 }
 
-def setColor(color){
-	log.debug "setColor >> ${state.id} >> ${color}"
-    def colors = color.hex
-    if(colors == null){
-        def srgb = hslToRGB(color.hue, color.saturation, 0.5)
-        def shexColor = rgbToHex(srgb)
-        colors = shexColor
-    }
-    
+def setColorTemperature(colortemperature){
     def body = [
         "id": state.id,
         "cmd": "color",
-        "data": colors,
+        "data": colortemperature + "K",
+        "subData": getDuration()
+    ]
+    def options = makeCommand(body)
+    sendCommand(options, null)
+    
+    setPowerByStatus(true)	
+}
+
+def setColor(color){
+	log.debug "setColor >> ${state.id} >> ${color}"
+
+	def hue = (color.hue != "NaN") ? color.hue : 13
+	def saturation = (color.saturation != "NaN") ? color.saturation : 13
+	def rgb = huesatToRGB(hue as Integer, saturation as Integer)
+	
+    def body = [
+        "id": state.id,
+        "cmd": "color",
+        "data": "rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})",
         "subData": getDuration()
     ]
     def options = makeCommand(body)
@@ -275,20 +216,6 @@ def processSetScene(data){
     sendCommand(options, null)
 }
 
-
-public String hslToHex(float hue, float saturation, float brightness) {
-    float h = Math.max( 0, Math.min( 360, hue ) )
-    float s = Math.max( 0, Math.min( 100, saturation ) )
-    float v = Math.max( 0, Math.min( 100, brightness ) )
-    h /= 360
-    s /= 100
-    v /= 100
-    
-    Color cc = Color.getHSBColor( h, s, v )
-
-    return String.format( "#%02x%02x%02x", cc.getRed(), cc.getGreen(), cc.getBlue() )
-}
-
 private hex(value, width=2) {
     def s = new BigInteger(Math.round(value).toString()).toString(16)
     while (s.size() < width) {
@@ -297,100 +224,6 @@ private hex(value, width=2) {
     return s
 }
 
-def rgbToHex(rgb) {
-    def r = hex(rgb.r)
-    def g = hex(rgb.g)
-    def b = hex(rgb.b)
-    def hexColor = "#${r}${g}${b}"
-    hexColor
-}
-
-def hslToRGB(var_h, var_s, var_l) {
-    float h = var_h / 100
-    float s = var_s / 100
-    float l = var_l
-
-    def r = 0
-    def g = 0
-    def b = 0
-
-    if (s == 0) {
-        r = l * 255
-        g = l * 255
-        b = l * 255
-    } else {
-        float var_2 = 0
-    	if (l < 0.5) {
-    		var_2 = l * (1 + s)
-    	} else {
-    		var_2 = (l + s) - (s * l)
-		}
-
-		float var_1 = 2 * l - var_2
-
-        r = 255 * hueToRgb(var_1, var_2, h + (1 / 3))
-        g = 255 * hueToRgb(var_1, var_2, h)
-        b = 255 * hueToRgb(var_1, var_2, h - (1 / 3))
-	}
-    
-    def rgb = [:]
-    rgb = [r: r as int, g: g as int, b: b as int]
-
-    return rgb
-}
-
-def hueToRgb(v1, v2, vh) {
-if (vh < 0) { vh += 1 }
-if (vh > 1) { vh -= 1 }
-if ((6 * vh) < 1) { return (v1 + (v2 - v1) * 6 * vh) }
-if ((2 * vh) < 1) { return (v2) }
-if ((3 * vh) < 2) { return (v1 + (v2 - v1) * ((2 / 3 - vh) * 6)) }
-return (v1)
-}
-
-def rgbToHSL(rgb) {
-    def colors = rgb.split(",")
-    
-    def r = colors[0].toInteger() / 255
-    def g = colors[1].toInteger() / 255
-    def b = colors[2].toInteger() / 255
-    def h = 0
-    def s = 0
-    def l = 0
-
-    def var_min = [r,g,b].min()
-    def var_max = [r,g,b].max()
-    def del_max = var_max - var_min
-
-    l = (var_max + var_min) / 2
-
-    if (del_max == 0) {
-        h = 0
-        s = 0
-    } else {
-    	if (l < 0.5) { s = del_max / (var_max + var_min) }
-    	else { s = del_max / (2 - var_max - var_min) }
-
-        def del_r = (((var_max - r) / 6) + (del_max / 2)) / del_max
-        def del_g = (((var_max - g) / 6) + (del_max / 2)) / del_max
-        def del_b = (((var_max - b) / 6) + (del_max / 2)) / del_max
-
-    	if (r == var_max) { h = del_b - del_g }
-        else if (g == var_max) { h = (1 / 3) + del_r - del_b }
-        else if (b == var_max) { h = (2 / 3) + del_g - del_r }
-
-        if (h < 0) { h += 1 }
-        if (h > 1) { h -= 1 }
-    }
-    
-    def hsl = [:]
-    hsl = [h: h * 100, s: s * 100, l: l]
-    
-//    sendEvent(name: "hue", value: h * 100, descriptionText: "Color has changed")
-//    sendEvent(name: "saturation", value: s * 100, descriptionText: "Color has changed", displayed: false)
-
-    hsl
-}
 
 def on(){
 	log.debug "Off >> ${state.id}"
@@ -415,17 +248,18 @@ def off(){
 }
 
 
-def updated() {}
+def updated() {
+	sendEvent("colorMode", "RGB")
+}
 
-def callback(physicalgraph.device.HubResponse hubResponse){
+def callback(hubitat.device.HubResponse hubResponse){
 	def msg
     try {
         msg = parseLanMessage(hubResponse.description)
 		def jsonObj = new JsonSlurper().parseText(msg.body)
         log.debug jsonObj
         
-     	String hex = String.format("#%02x%02x%02x", jsonObj.state.colorRGB.red, jsonObj.state.colorRGB.blue, jsonObj.state.colorRGB.green);
-        sendEvent(name:"color", value: hex )
+        sendEvent(name:"color", value: jsonObj.properties.color)
         sendEvent(name:"level", value: jsonObj.properties.brightness)
         sendEvent(name:"switch", value: jsonObj.properties.power == true ? "on" : "off")
 	    
@@ -438,7 +272,7 @@ def callback(physicalgraph.device.HubResponse hubResponse){
 
 
 def sendCommand(options, _callback){
-	def myhubAction = new physicalgraph.device.HubAction(options, null, [callback: _callback])
+	def myhubAction = new hubitat.device.HubAction(options, null, [callback: _callback])
     sendHubCommand(myhubAction)
 }
 
@@ -485,7 +319,6 @@ def timer(){
         }
         updateTimer()
     }
-//	log.debug "Left Time >> ${state.timerCount}"
 }
 
 def updateTimer(){
@@ -504,7 +337,6 @@ def processTimer(second){
     }else{
     	state.timerCount = second
     }
-//    log.debug "Left Time >> ${state.timerCount} seconds"
     updateTimer()
 }
 
@@ -535,4 +367,21 @@ def getDuration(){
         }
     }
     return duration
+}
+
+def huesatToRGB(float hue, float sat) {
+	while(hue >= 100) hue -= 100
+	int h = (int)(hue / 100 * 6)
+	float f = hue / 100 * 6 - h
+	int p = Math.round(255 * (1 - (sat / 100)))
+	int q = Math.round(255 * (1 - (sat / 100) * f))
+	int t = Math.round(255 * (1 - (sat / 100) * (1 - f)))
+	switch (h) {
+		case 0: return [255, t, p]
+		case 1: return [q, 255, p]
+		case 2: return [p, 255, t]
+		case 3: return [p, q, 255]
+		case 4: return [t, p, 255]
+		case 5: return [255, p, q]
+	}
 }
