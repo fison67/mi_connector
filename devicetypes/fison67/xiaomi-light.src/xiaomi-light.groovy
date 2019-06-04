4a8d495dfc81f94de90eb1102e58c6ce8187f8f0
chals <fison67@nate.com>
2019-06-04 20:43:11 +0900

    Update xiaomi-light.groovy

--- devicetypes/fison67/xiaomi-light.src/xiaomi-light.groovy
+++ devicetypes/fison67/xiaomi-light.src/xiaomi-light.groovy
@@ -1,5 +1,5 @@
 /**
- *  Xiaomi Light (v.0.0.3)
+ *  Xiaomi Light (v.0.0.4)
  *
  * MIT License
  *
@@ -36,7 +36,7 @@ import java.awt.Color
 import java.util.ArrayList
 
 metadata {
-	definition (name: "Xiaomi Light", namespace: "fison67", author: "fison67", vid: "generic-rgb-color-bulb", ocfDeviceType: "oic.d.light") {
+	definition (name: "Xiaomi Light", namespace: "fison67", author: "fison67", vid: "SmartThings-smartthings-Simulated_RGBW_Bulb", ocfDeviceType: "oic.d.light") {
         capability "Switch"						//"on", "off"
         capability "Actuator"
         capability "Refresh"
@@ -44,12 +44,9 @@ metadata {
 		capability "Color Control"
 		capability "ColorTemperature"
         capability "Switch Level"
-        capability "Health Check"
-        capability "Configuration"
 
         attribute "lastOn", "string"
         attribute "lastOff", "string"
-        
         attribute "lastCheckin", "Date"
          
         command "setTimeRemaining"
@@ -144,7 +141,7 @@ def setInfo(String app_url, String id) {
 }
 
 def setStatus(params){
-//	log.debug "Status >> ${params}"
+	log.debug "Status >> ${params}"
     def now = new Date().format("yyyy-MM-dd HH:mm:ss", location.timeZone)
  	switch(params.key){
     case "power":
@@ -158,8 +155,12 @@ def setStatus(params){
     	break;
     case "color":
     	def colors = params.data.split(",")
-        String hex = String.format("#%02x%02x%02x", colors[0].toInteger(), colors[1].toInteger(), colors[2].toInteger());  
-    	sendEvent(name:"color", value: hex )
+    	sendEvent(name:"color", value: String.format("#%02x%02x%02x", colors[0].toInteger(), colors[1].toInteger(), colors[2].toInteger())  )
+        
+        float[] hsbValues = new float[3];
+		def hueSat = Color.RGBtoHSB(colors[0].toInteger(), colors[1].toInteger(), colors[2].toInteger(),hsbValues)
+    	sendEvent(name:"hue", value: (hueSat[0] * 100) as int  )
+    	sendEvent(name:"saturation", value: (hueSat[1] * 100) as int)
     	break;
     case "brightness":
     	sendEvent(name:"level", value: params.data )
@@ -205,9 +206,8 @@ def setColor(color){
 	log.debug "setColor >> ${state.id} >> ${color}"
     def colors = color.hex
     if(colors == null){
-        def srgb = hslToRGB(color.hue, color.saturation, 0.5)
-        def shexColor = rgbToHex(srgb)
-        colors = shexColor
+    	def rgb = huesatToRGB(color.hue as Integer, color.saturation as Integer)
+        colors = "rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})"
     }
     
     def body = [
@@ -235,120 +235,12 @@ def setColorTemperature(colortemperature){
     setPowerByStatus(true)	
 }
 
-public String hslToHex(float hue, float saturation, float brightness) {
-    float h = Math.max( 0, Math.min( 360, hue ) )
-    float s = Math.max( 0, Math.min( 100, saturation ) )
-    float v = Math.max( 0, Math.min( 100, brightness ) )
-    h /= 360
-    s /= 100
-    v /= 100
-    
-    Color cc = Color.getHSBColor( h, s, v )
-
-    return String.format( "#%02x%02x%02x", cc.getRed(), cc.getGreen(), cc.getBlue() )
-}
-
 private hex(value, width=2) {
     def s = new BigInteger(Math.round(value).toString()).toString(16)
     while (s.size() < width) {
     	s = "0" + s
     }
     return s
-}
-
-def rgbToHex(rgb) {
-    def r = hex(rgb.r)
-    def g = hex(rgb.g)
-    def b = hex(rgb.b)
-    def hexColor = "#${r}${g}${b}"
-    hexColor
-}
-
-def hslToRGB(var_h, var_s, var_l) {
-    float h = var_h / 100
-    float s = var_s / 100
-    float l = var_l
-
-    def r = 0
-    def g = 0
-    def b = 0
-
-    if (s == 0) {
-        r = l * 255
-        g = l * 255
-        b = l * 255
-    } else {
-        float var_2 = 0
-    	if (l < 0.5) {
-    		var_2 = l * (1 + s)
-    	} else {
-    		var_2 = (l + s) - (s * l)
-		}
-
-		float var_1 = 2 * l - var_2
-
-        r = 255 * hueToRgb(var_1, var_2, h + (1 / 3))
-        g = 255 * hueToRgb(var_1, var_2, h)
-        b = 255 * hueToRgb(var_1, var_2, h - (1 / 3))
-	}
-    
-    def rgb = [:]
-    rgb = [r: r as int, g: g as int, b: b as int]
-
-    return rgb
-}
-
-def hueToRgb(v1, v2, vh) {
-if (vh < 0) { vh += 1 }
-if (vh > 1) { vh -= 1 }
-if ((6 * vh) < 1) { return (v1 + (v2 - v1) * 6 * vh) }
-if ((2 * vh) < 1) { return (v2) }
-if ((3 * vh) < 2) { return (v1 + (v2 - v1) * ((2 / 3 - vh) * 6)) }
-return (v1)
-}
-
-def rgbToHSL(rgb) {
-    def colors = rgb.split(",")
-    
-    def r = colors[0].toInteger() / 255
-    def g = colors[1].toInteger() / 255
-    def b = colors[2].toInteger() / 255
-    def h = 0
-    def s = 0
-    def l = 0
-
-    def var_min = [r,g,b].min()
-    def var_max = [r,g,b].max()
-    def del_max = var_max - var_min
-
-    l = (var_max + var_min) / 2
-
-    if (del_max == 0) {
-        h = 0
-        s = 0
-    } else {
-    	if (l < 0.5) { s = del_max / (var_max + var_min) }
-    	else { s = del_max / (2 - var_max - var_min) }
-
-        def del_r = (((var_max - r) / 6) + (del_max / 2)) / del_max
-        def del_g = (((var_max - g) / 6) + (del_max / 2)) / del_max
-        def del_b = (((var_max - b) / 6) + (del_max / 2)) / del_max
-
-    	if (r == var_max) { h = del_b - del_g }
-        else if (g == var_max) { h = (1 / 3) + del_r - del_b }
-        else if (b == var_max) { h = (2 / 3) + del_g - del_r }
-
-        if (h < 0) { h += 1 }
-        if (h > 1) { h -= 1 }
-    }
-    
-    def hsl = [:]
-    hsl = [h: h * 100, s: s * 100, l: l]
-    
-//    sendEvent(name: "hue", value: h * 100, descriptionText: "Color has changed")
-//    sendEvent(name: "saturation", value: s * 100, descriptionText: "Color has changed", displayed: false)
-
-    hsl
 }
 
 def on(){
@@ -383,7 +275,10 @@ def callback(physicalgraph.device.HubResponse hubResponse){
 		def jsonObj = new JsonSlurper().parseText(msg.body)
         log.debug jsonObj
         
-     	String hex = String.format("#%02x%02x%02x", jsonObj.state.colorRGB.red, jsonObj.state.colorRGB.blue, jsonObj.state.colorRGB.green);
+     	String hex = String.format("#%02x%02x%02x", jsonObj.state.colorRGB.red, jsonObj.state.colorRGB.blue, jsonObj.state.colorRGB.green)
+    	sendEvent(name:"hue", value: jsonObj.state.colorHue  )
+    	sendEvent(name:"saturation", value: jsonObj.state.colorSaturation)
+    	sendEvent(name:"colorTemperature", value: jsonObj.state.colorTemperature)
         sendEvent(name:"color", value: hex )
         sendEvent(name:"level", value: jsonObj.properties.brightness)
         sendEvent(name:"switch", value: jsonObj.properties.power == true ? "on" : "off")
@@ -496,3 +391,19 @@ def getDuration(){
     return duration
 }
 
+def huesatToRGB(float hue, float sat) {
+	while(hue >= 100) hue -= 100
+	int h = (int)(hue / 100 * 6)
+	float f = hue / 100 * 6 - h
+	int p = Math.round(255 * (1 - (sat / 100)))
+	int q = Math.round(255 * (1 - (sat / 100) * f))
+	int t = Math.round(255 * (1 - (sat / 100) * (1 - f)))
+	switch (h) {
+		case 0: return [255, t, p]
+		case 1: return [q, 255, p]
+		case 2: return [p, 255, t]
+		case 3: return [p, q, 255]
+		case 4: return [t, p, 255]
+		case 5: return [255, p, q]
+	}
+}
