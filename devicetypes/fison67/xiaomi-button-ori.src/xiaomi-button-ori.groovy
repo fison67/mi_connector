b15134a367e9a3238621a4cf779853d8d558d158
chals <fison67@nate.com>
2019-09-20 13:27:00 +0900

    Update xiaomi-button-ori.groovy

--- devicetypes/fison67/xiaomi-button-ori.src/xiaomi-button-ori.groovy
+++ devicetypes/fison67/xiaomi-button-ori.src/xiaomi-button-ori.groovy
@@ -103,7 +103,7 @@ def refresh(){
      	"method": "GET",
         "path": "/devices/get/${state.id}",
         "headers": [
-        	"HOST": state.app_url,
+        	"HOST": parent._getServerURL(),
             "Content-Type": "application/json"
         ]
     ]
@@ -146,7 +146,7 @@ def makeCommand(body){
      	"method": "POST",
         "path": "/control",
         "headers": [
-        	"HOST": state.app_url,
+        	"HOST": parent._getServerURL(),
             "Content-Type": "application/json"
         ],
         "body":body
