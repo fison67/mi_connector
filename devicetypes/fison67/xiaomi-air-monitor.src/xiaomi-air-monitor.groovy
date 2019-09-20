9688ffa4fba0b825a6bbc48967c3f0cf5fb8d65a
chals <fison67@nate.com>
2019-09-20 13:25:25 +0900

    Update xiaomi-air-monitor.groovy

--- devicetypes/fison67/xiaomi-air-monitor.src/xiaomi-air-monitor.groovy
+++ devicetypes/fison67/xiaomi-air-monitor.src/xiaomi-air-monitor.groovy
@@ -417,7 +417,7 @@ def refresh(){
      	"method": "GET",
         "path": "/devices/get/${state.id}",
         "headers": [
-        	"HOST": state.app_url,
+        	"HOST": parent._getServerURL(),
             "Content-Type": "application/json"
         ]
     ]
@@ -613,7 +613,7 @@ def makeCommand(body){
      	"method": "POST",
         "path": "/control",
         "headers": [
-        	"HOST": state.app_url,
+        	"HOST": parent._getServerURL(),
             "Content-Type": "application/json"
         ],
         "body":body
