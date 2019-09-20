d7710ae183aff1c388d4dbbfdca79253b638da92
chals <fison67@nate.com>
2019-09-20 13:24:44 +0900

    Update xiaomi-air-fresh.groovy

--- devicetypes/fison67/xiaomi-air-fresh.src/xiaomi-air-fresh.groovy
+++ devicetypes/fison67/xiaomi-air-fresh.src/xiaomi-air-fresh.groovy
@@ -342,7 +342,7 @@ def refresh(){
      	"method": "GET",
         "path": "/devices/get/${state.id}",
         "headers": [
-        	"HOST": state.app_url,
+        	"HOST": parent._getServerURL(),
             "Content-Type": "application/json"
         ]
     ]
@@ -496,7 +496,7 @@ def makeCommand(body){
      	"method": "POST",
         "path": "/control",
         "headers": [
-        	"HOST": state.app_url,
+        	"HOST": parent._getServerURL(),
             "Content-Type": "application/json"
         ],
         "body":body
