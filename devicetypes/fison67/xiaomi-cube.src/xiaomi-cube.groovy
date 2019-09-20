3d05259a18c2d883e902712261814317d573f5b7
chals <fison67@nate.com>
2019-09-20 13:28:19 +0900

    Update xiaomi-cube.groovy

--- devicetypes/fison67/xiaomi-cube.src/xiaomi-cube.groovy
+++ devicetypes/fison67/xiaomi-cube.src/xiaomi-cube.groovy
@@ -149,7 +149,7 @@ def refresh(){
      	"method": "GET",
         "path": "/devices/get/${state.id}",
         "headers": [
-        	"HOST": state.app_url,
+        	"HOST": parent._getServerURL(),
             "Content-Type": "application/json"
         ]
     ]
@@ -183,7 +183,7 @@ def makeCommand(body){
      	"method": "POST",
         "path": "/control",
         "headers": [
-        	"HOST": state.app_url,
+        	"HOST": parent._getServerURL(),
             "Content-Type": "application/json"
         ],
         "body":body
