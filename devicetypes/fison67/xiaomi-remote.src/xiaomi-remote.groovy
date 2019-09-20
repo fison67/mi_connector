23e5db0f7cabaeb3e2c9abf195917812a062d8b1
chals <fison67@nate.com>
2019-09-20 13:40:35 +0900

    Update xiaomi-remote.groovy

--- devicetypes/fison67/xiaomi-remote.src/xiaomi-remote.groovy
+++ devicetypes/fison67/xiaomi-remote.src/xiaomi-remote.groovy
@@ -90,7 +90,7 @@ def makeCommand(body){
      	"method": "POST",
         "path": "/control",
         "headers": [
-        	"HOST": state.app_url,
+        	"HOST": parent._getServerURL(),
             "Content-Type": "application/json"
         ],
         "body":body
