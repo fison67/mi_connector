e70686958fe5a84b638e6e0e750c5a5fa68f9897
chals <fison67@nate.com>
2019-09-20 13:43:41 +0900

    Update xiaomi-vacuums2.groovy

--- devicetypes/fison67/xiaomi-vacuums2.src/xiaomi-vacuums2.groovy
+++ devicetypes/fison67/xiaomi-vacuums2.src/xiaomi-vacuums2.groovy
@@ -308,7 +308,7 @@ def refresh(){
      	"method": "GET",
         "path": "/devices/get/${state.id}",
         "headers": [
-        	"HOST": state.app_url,
+        	"HOST": parent._getServerURL(),
             "Content-Type": "application/json"
         ]
     ]
@@ -549,7 +549,7 @@ def makeCommand(body){
      	"method": "POST",
         "path": "/control",
         "headers": [
-        	"HOST": state.app_url,
+        	"HOST": parent._getServerURL(),
             "Content-Type": "application/json"
         ],
         "body":body
